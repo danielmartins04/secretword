8900afb6157c9e149eba0ec9e557fc9bab015e91
Francisco Daniel Freitas Martins <francisco.daniel@ambevtech.com.br>
2024-07-02 19:36:48 -0300

    start screen created

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret word</h1>
+        <p>Clique para começar a jogar</p>
+        <button>Começar o jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
